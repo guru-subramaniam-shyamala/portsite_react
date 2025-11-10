090eafc4e367f2f0b07d462ffca68ac1bbb9d2c0
guru-subramaniam-shyamala <saiguru0421@gmail.com>
2025-11-09 20:03:56 -0500

    update src

--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -4,35 +4,17 @@ import "./WorkExperience.css";
 const WorkExperience = () => {
   const experiences = [
     {
+      company: "Cprime Technologies",
+      duration: "Jan 2018 - Nov 2018",
+      role: "Junior Software Engineer",
+      description: "Developed and optimized enterprise web applications using ReactJS, CSS, and MySQL, improving performance and stability by 15%. Collaborated in Agile sprints to automate testing and streamline deployments, reducing QA time by 25%.",
+    },
+    {
       company: "Vosyn",
       duration: "April 2024 - Present",
       role: "Cloud Engineer",
       description: "Designed and implemented scalable cloud infrastructure across AWS and GCP. Automated deployments using Terraform and GitHub Actions, migrated services to GKE, and configured load balancers for high availability. Enhanced security with IAM policies and integrated monitoring with Cloud Monitoring and Grafana.",
-    },
-    // {
-    //   company: "Verisk",
-    //   duration: "Mar 2024 - Present",
-    //   role: "Cloud/DevOps Engineer",
-    //   description: " I manage AWS infrastructure using CloudFormation, Lambda, and Fargate, automate security and compliance with Wiz, GuardDuty, and AWS Security Hub, and optimize CI/CD pipelines with Jenkins and Kubernetes.",
-    // },
-    // {
-    //   company: "Nike",
-    //   duration: "Jan 2023 – Feb 2024",
-    //   role: "AWS/DevOps Engineer",
-    //   description: "I automated cloud infrastructure using AWS, Terraform, Ansible, and Jenkins, optimized security and CI/CD pipelines, and managed Kubernetes deployments for efficient cloud operations.",
-    // },
-    // {
-    //   company: "TCS",
-    //   duration: "May 2022  – Nov 2022",
-    //   role: " DevOps Engineer  ",
-    //   description: "I automated CI/CD pipelines using Jenkins, Git, Terraform, and Puppet, managed AWS infrastructure, and optimized deployment processes for seamless cloud operations.",
-    // },
-    // {
-    //   company: "Syntel",
-    //   duration: "Mar 2021 – Apr 2022",
-    //   role: "Build and Release Engineer",
-    //   description: "I managed CI/CD pipelines using Jenkins, Maven, SVN, and Git, ensuring smooth software releases while automating deployment processes across Windows and Linux environments.",
-    // },
+    }
   ];
 
   return (
